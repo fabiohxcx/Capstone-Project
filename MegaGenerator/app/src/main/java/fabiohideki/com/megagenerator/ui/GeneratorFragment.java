17c95ce7c4ec911f194cea8919b8c12ded6ea3dd
fabio.lagoa <fabio.lagoa@lge.com>
2018-02-07 13:20:51 -0200

    creating buttons for generator screem

--- MegaGenerator/app/src/main/java/fabiohideki/com/megagenerator/ui/GeneratorFragment.java
+++ MegaGenerator/app/src/main/java/fabiohideki/com/megagenerator/ui/GeneratorFragment.java
@@ -3,10 +3,16 @@ package fabiohideki.com.megagenerator.ui;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import fabiohideki.com.megagenerator.R;
 
 /**
@@ -14,6 +20,8 @@ import fabiohideki.com.megagenerator.R;
  */
 public class GeneratorFragment extends Fragment {
 
+    @BindView(R.id.card_result)
+    CardView cardViewResult;
 
     public GeneratorFragment() {
         // Required empty public constructor
@@ -21,10 +29,25 @@ public class GeneratorFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_generator, container, false);
+
+        ButterKnife.bind(this, rootView);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_generator, container, false);
+        return rootView;
+    }
+
+    @OnClick(R.id.bt_generate)
+    public void generate(View view) {
+
+        Animation slideUp = AnimationUtils.loadAnimation(getContext(), R.anim.slide_up);
+
+        if (cardViewResult.getVisibility() == View.GONE) {
+            cardViewResult.setVisibility(View.VISIBLE);
+            cardViewResult.startAnimation(slideUp);
+        }
+
     }
 
 }
